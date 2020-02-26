84c5ebbb552571963a0458da34f248673b0b2800
MMarek <mildnermarek@gmail.com>
2020-02-26 20:22:20 +0100

    login +

--- src/components/task04.js
+++ src/components/task04.js
@@ -27,5 +27,34 @@ class Login extends Component {
         if (login.length <= 2) {
             errors.push('Login too short');
         }
+
+        if (password.length <= 4) {
+            errors.push('Password too short');
+        }
+
+        this.setState({errors})
+
+        if (errors.length === 0) {
+            this.login();
+        }
+    };
+
+    login() {
+        const {login, password} = this.state;
+        FakeAPI.login({
+            username: login,
+            password: password
+        }).then(user => {
+            console.log(user);
+        }).catch(err => {
+            this.setState({errors: [err]});
+        })
+    }
+
+    render() {
+        const {user} = this.state;
+        if (user) {
+            return ()
+        }
     }
 }
