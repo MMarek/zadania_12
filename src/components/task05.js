810134c44171782044bf1b75e36646071b7d17dc
MMarek <mildnermarek@gmail.com>
2020-03-10 23:28:58 +0100

    to do list

---
+++ src/components/task05.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+
+class ToDoList extends Component {
+    state = {};
+
+    handleTodoChange = (e) => {
+        this.setState()
+    };
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        };
+
+    render() {
+        return
+    }
+}
+
+
+function Task05() {
+    return <ToDoList/>
+}
+
+export default Task05
