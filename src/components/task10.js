2d00d9230b7b19a478e305521233e876aca93ab8
MMarek <mildnermarek@gmail.com>
2020-03-11 17:50:33 +0100

    bulb exercise

---
+++ src/components/task10.js
@@ -0,0 +1,62 @@
+import React, {Component} from "react";
+
+class Bulb extends Component {
+    state = {
+        bulbOn: false
+    };
+
+    handleTurnOffClick = () => {
+        this.setState({
+            bulbOn: false
+        });
+    };
+
+    handleTurnOnClick = () => {
+        this.setState({
+            bulbOn: true
+        });
+    };
+
+    render() {
+        let bulb;
+        if (this.state.bulbOn) {
+            bulb = <BulbOn onClick={this.handleTurnOffClick}/>;
+        } else {
+            bulb = <BulbOff onClick={this.handleTurnOnClick}/>;
+        }
+        return (
+            <div>
+                <h1>Żarówka</h1>
+                {bulb}
+            </div>
+        );
+    }
+}
+
+class BulbOff extends Component {
+    render() {
+        return (
+            <>
+                <span>Żarówka zgaszona</span>
+                <button onClick={this.props.onClick}>Włącz</button>
+            </>
+        );
+    }
+}
+
+class BulbOn extends Component {
+    render() {
+        return (
+            <>
+                <strong>Żarówka świeci</strong>
+                <button onClick={this.props.onClick}>Wyłącz</button>
+            </>
+        );
+    }
+}
+
+function Task10() {
+    return <Bulb/>;
+}
+
+export default Task10;
