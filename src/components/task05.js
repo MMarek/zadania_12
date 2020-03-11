efb4ffda4cee50b1e4ad62a9ef161b8170c92504
MMarek <mildnermarek@gmail.com>
2020-03-11 10:27:12 +0100

    to do list correction

--- src/components/task05.js
+++ src/components/task05.js
@@ -2,15 +2,42 @@ import React, {Component} from "react";
 
 
 class ToDoList extends Component {
-    state = {};
+    state = {
+        tasks: [],
+        todo: ''
+    };
 
     handleTodoChange = (e) => {
-        this.setState()
+        this.setState({
+            todo: e.target.value
+        })
     };
 
     handleSubmit = (e) => {
         e.preventDefault();
+        let newTask = {
+            id: nextId++,
+            name: this.state.todo,
+            done: false
         };
+        let updatedTask = [...this.state.tasks, newTask]
+        this.setState({
+            tasks: updatedTask,
+            todo: ''
+        })
+    };
+
+    handleToggleDone = (id) => {
+        let updatedTasks = this.state.tasks.map(task => {
+            if (task.id === id) {
+                task.done = !task.done;
+            }
+            return task;
+        });
+        this.setState({
+            tasks: updatedTasks
+        })
+    };
 
     render() {
         return
